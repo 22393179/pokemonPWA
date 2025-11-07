f45204c83dc003cc36300c5fe6351401d8019468
Miguel Ortega <miguelangelortegamazun@gmail.com>
2025-11-06 19:57:18 -0500

    nuevo

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,10 @@
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SHOW_NOTIFICATION') {
-    const { title, body, icon, vibrate, tag } = event.data.options || {};
-    self.registration.showNotification(title || 'Pokédex actualizada', {
-      body: body || 'Atrápalos ya',
-      icon: icon || '/icons/icon-192x192.png',
-      vibrate: vibrate || [200, 100, 200],
-      tag: tag || 'poke-notify',
-    });
-  }
+// service-worker.js
+self.addEventListener('install', () => {
+  console.log('Service Worker instalado.');
 });
+
+self.addEventListener('activate', () => {
+  console.log('Service Worker activado.');
+});
+
+// No hace falta escuchar mensajes si usamos showNotification()
